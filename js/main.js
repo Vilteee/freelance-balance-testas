677473a410078f2286da561802cafb9d7b898efa
PavasarioSniegas <vilte.kar@gmail.com>
2023-05-29 09:26:39 +0300

    creating all months array

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const allMonths = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'Nowember',
+    'December',
+];
